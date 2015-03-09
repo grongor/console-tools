83c774a5dc5fff3d87061781d6f1b30f52101406
Jakub Chabek <jakub.chabek@heureka.cz>
2015-03-09 08:32:04 +0100

    Adjusted brm

--- git-branch-remove.sh
+++ git-branch-remove.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-git branch --merged | grep -v '\\*' | xargs -n 1 git branch -d
+branches=`git branch --merged | grep -v '*' | xargs -n 1`
+count=`echo "$branches" | wc -w`
+
+if [[ $count -ne 0 ]]; then
+    git branch -d $branches
+else
+    echo "There aren't any merged branches to remove"
+fi
+
